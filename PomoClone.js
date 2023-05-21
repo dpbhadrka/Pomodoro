80251b100830177dd959f39a9d00d5e8f929de29
Divyesh Bhadrka <p.bhadrka7@gecg28.ac.in>
2023-05-21 23:53:35 +0530

    New audio added.

--- PomoClone.js
+++ PomoClone.js
@@ -1,3 +1,34 @@
+//Call below function when click on Pomodoro
+function pomoDoro() {
+  if (seconds < 60) {
+    alert("The timer is still running, are you sure you want to switch?");
+  }
+
+  //Changing the qoute.
+  document.getElementById("quote").innerHTML = "#Time to focus!";
+  document.getElementsByTagName("title")[0].innerHTML =
+    "25:00 - Time to focus!";
+
+  //Changing the bg-color.
+  document.body.style.backgroundColor = "#1A1A2E";
+
+  //Adding the active clss and remove the class from other if available.
+  document.getElementById("first").classList.add("active");
+  document.getElementById("third").classList.remove("active");
+  document.getElementById("second").classList.remove("active");
+
+  document.getElementById("Seconds").innerHTML = "00";
+  document.getElementById("clickMe").innerHTML = "Start";
+  clearInterval(stop);
+
+  //Variables according to the pomodoro timer.
+  document.getElementById("Minutes").innerHTML = 25;
+  originalSeconds = 60;
+  minutes = 25;
+  seconds = 60;
+  originalMinutes = 25;
+}
+
 //Call below function when click on Short Break
 function shortBreak() {
   if (seconds < 60) {
@@ -7,12 +38,17 @@ function shortBreak() {
   document.getElementsByTagName("title")[0].innerHTML =
     "05:00- Time for a break!!";
 
+  //Adding active class and remove from other.
   document.getElementById("third").classList.remove("active");
   document.getElementById("first").classList.remove("active");
   document.getElementById("second").classList.add("active");
-  document.body.style.backgroundColor = "rgb(56, 13, 38)";
+
+  //Changing background color.
+  document.body.style.backgroundColor = "#16213E";
   document.getElementById("Seconds").innerHTML = "00";
   document.getElementById("clickMe").innerHTML = "Start";
+
+  //Variables according to the shortbreak.
   clearInterval(stop);
   originalSeconds = 60;
   seconds = 60;
@@ -26,43 +62,29 @@ function longBreak() {
   if (seconds < 60) {
     alert("The timer is still running, are you sure you want to switch?");
   }
+
+  //Changing the quote.
   document.getElementById("quote").innerHTML = "#Time for a break!!";
   document.getElementsByTagName("title")[0].innerHTML =
     "15:00 - Time for a break!!";
+
+  //Adding the active class and remove from other.
   document.getElementById("second").classList.remove("active");
   document.getElementById("first").classList.remove("active");
   document.getElementById("third").classList.add("active");
-  document.body.style.backgroundColor = "rgb(57, 44, 90)";
+
+  //Changing Background color.
+  document.body.style.backgroundColor = "#0F3460";
   document.getElementById("clickMe").innerHTML = "Start";
   document.getElementById("Seconds").innerHTML = "00";
   clearInterval(stop);
+
+  //Variables according to the Longbreak.
   document.getElementById("Minutes").innerHTML = 15;
   originalSeconds = 60;
   seconds = 60;
   originalMinutes = 15;
   minutes = 15;
-}
-
-//Call below function when click on Pomodoro
-function pomoDoro() {
-  if (seconds < 60) {
-    alert("The timer is still running, are you sure you want to switch?");
-  }
-  document.getElementById("quote").innerHTML = "#Time to focus!";
-  document.getElementsByTagName("title")[0].innerHTML =
-    "25:00 - Time to focus!";
-  document.getElementById("first").classList.add("active");
-  document.body.style.backgroundColor = "#0f182a";
-  document.getElementById("third").classList.remove("active");
-  document.getElementById("second").classList.remove("active");
-  document.getElementById("Seconds").innerHTML = "00";
-  document.getElementById("clickMe").innerHTML = "Start";
-  clearInterval(stop);
-  document.getElementById("Minutes").innerHTML = 25;
-  originalSeconds = 60;
-  minutes = 25;
-  seconds = 60;
-  originalMinutes = 25;
 }
 
 //Real function of Pomodoro
@@ -73,7 +95,8 @@ var seconds = 60;
 let stop;
 let startAudio = new Audio("Start.mp3");
 let pauseAudio = new Audio("Stop.mp3");
-let timerCompleted = new Audio("FinalRingingSound.mp3");
+// let timerCompleted = new Audio("FinalRingingSound.mp3");
+let timerCompleted = new Audio("Pomodoro-Complete-Sound.mp3");
 
 //Call below function when click on start button
 function mainFunction() {
